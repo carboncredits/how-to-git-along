5773667dcdf31473e48f28953ab342e1d99fe490
patricoferris <patricoferris@users.noreply.github.com>
2024-08-06 09:22:27 +0000

    deploy: b3be646fc1f7b7c0818f1a22ed7abbae816a9608

---
+++ assets/slidev/section-7_dsod-F.js
@@ -0,0 +1,1 @@
+import{o as s,b as o,e as t,f as a}from"../modules/vue-rOHTgtFx.js";import{u as r}from"./context-o1RxciAU.js";const c={class:"slidev-layout h-full grid section"},l={class:"my-auto"},u={__name:"section",setup(n){return r(),(e,i)=>(s(),o("div",c,[t("div",l,[a(e.$slots,"default")])]))}};export{u as _};
